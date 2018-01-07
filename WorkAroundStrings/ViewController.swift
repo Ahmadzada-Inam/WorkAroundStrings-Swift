194de8b62889017e4d38dc5177a036a7df37635b
Inam Ahmad-zada <onedev@Inams-MacBook-Pro.local>
2018-01-07 08:21:54 -0500

    Added a way to handle the conversion between data and string

--- WorkAroundStrings/ViewController.swift
+++ WorkAroundStrings/ViewController.swift
@@ -13,9 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Intro
         let string = "Toronto is very cold today"
         print(string.count)
         print(string[string.index(string.startIndex, offsetBy: 8)])
+        
+        //String to data and vice versa
+        let ellipsis: UInt16 = 0x2026
+        let theString = String(format: "\(string)%C", arguments: [ellipsis])
+        let asciiData = theString.data(using: .ascii, allowLossyConversion: true)
+        if let data = asciiData {
+            let asciiString = String(data: data, encoding: .ascii)!
+            print("Original: \(theString), \(theString.count)")
+            print("Modified: \(asciiString), \(asciiString.count)")
+        }
     }
 }
 
